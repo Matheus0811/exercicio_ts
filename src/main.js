470eda3c646849af80441a6acb10d9fb9a50c05e
Matheus0811 <matheusbg0811@gmail.com>
2024-02-05 09:44:13 -0300

    type

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(x, y) {
+    return x * y;
+}
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log(resultadoMultiplicacao);
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+const mensagemSaudacao = saudacao("João");
+console.log(mensagemSaudacao);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+function multiplicacao(x: number, y: number): number {
+    return x * y;
+}
+
+const resultadoMultiplicacao: number = multiplicacao(5, 3);
+console.log(resultadoMultiplicacao);
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+const mensagemSaudacao: string = saudacao("João");
+console.log(mensagemSaudacao); 
